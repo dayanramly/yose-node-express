b1958419a464c35955bd878f670418f5e32329de
Manggala Pramuditya Wiryawan <manggala.wiryawan@gmail.com>
2016-10-29 10:58:04 +0700

    Power of Two gan...

--- libs/world.prime.factors/lib/prime.factors.of.js
+++ libs/world.prime.factors/lib/prime.factors.of.js
@@ -1,6 +1,20 @@
 primeFactorsOf = function(number) {
-	var factors = [];
-	return factors;
+
+	number = Math.floor(number);
+    var root, factors = [], x, sqrt = Math.sqrt, doLoop = 1 < number;
+    while( doLoop ){
+        root = sqrt(number);
+        x = 2;
+        if (number % x) {
+            x = 3;
+            while ((number % x) && ((x += 2) < root));
+        }
+        x = (x > root) ? number : x;
+        factors.push(x);
+        doLoop = ( x != number );
+        number /= x;
+    }
+    return factors;
 };
 
 module.exports = primeFactorsOf;
