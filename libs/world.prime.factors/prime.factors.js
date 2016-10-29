4399686b736696a3cf12b2ea9f628cc399085762
Manggala Pramuditya Wiryawan <manggala.wiryawan@gmail.com>
2016-10-29 14:28:41 +0700

    String Guard

--- libs/world.prime.factors/prime.factors.js
+++ libs/world.prime.factors/prime.factors.js
@@ -2,11 +2,21 @@ var primeFactorsOf = require('./lib/prime.factors.of');
 
 var primeFactors = function(request, response) {
     
-	var number = parseInt(request.query['number']);
-    var decomposition = primeFactorsOf(number);
+	var number = request.query['number'];
 
-	response.setHeader('Content-Type', 'application/json');
-    response.send(JSON.stringify({ number: number, decomposition: decomposition }));
+    if(isNaN(number))
+    {
+    	var result = { number: number, error: 'not a number' };
+
+    } else {
+
+    	var number = parseInt(number);
+    	var decomposition = primeFactorsOf(number);
+    	var result = { number: number, decomposition: decomposition };
+    }
+
+    	response.setHeader('Content-Type', 'application/json');
+	    response.send(JSON.stringify(result));
 };
 
 module.exports = primeFactors;
--- test/world.prime.factors/challenge.power.of.two.spec.js
+++ test/world.prime.factors/challenge.power.of.two.spec.js
@@ -28,5 +28,18 @@ describe('Passing the Power of Two level:', function() {
             done(); 
         });
     });
+
+    it('returns error when not a number', function(done) {
+        request('http://localhost:7000/primeFactors?number=hello', function(error, response, body) {
+            expect(body).to.equal( JSON.stringify( 
+                {
+                    "number" : "hello",
+                    "error" : "not a number"
+                } 
+            ));
+            
+            done(); 
+        });
+    });
    
 });
